ca138914b9b76850de7542aa9c60cba7e99b6a16
Nasq23 <honey.menu23@gmail.com>
2019-09-04 15:25:04 -0400

    variable exercise

---
+++ src/com/company/HelloWorld.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class HelloWorld {
+
+
+    public static void main( String[] args ) {
+        //declare variables here before we use them
+        int mynumber;
+        double myothernumber;
+        //all three variables will be declared as integers
+
+        mynumber = 2;
+        myothernumber = 1.7938; //you could also use the constant Integer.MAX_VALUE
+
+
+
+        //Use the variables here
+        System.out.println("My number is 2");
+        System.out.println("My other number is 1.7938");
+    }
+}
